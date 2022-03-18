346f70a832edd1094d47d6e9ee63a7de8d03847b
Mateus Joaquim <mateus.joaquim@tpf.local>
2022-03-18 14:18:53 -0300

    Aulas Python

---
+++ Aula2.py
@@ -0,0 +1,9 @@
+python
+
+a = 10
+b = 15
+soma = a + b
+subtracao = a - b
+multiplicacao = a * b
+divisao = a / b
+print(soma)
--- README.md
+++
@@ -1,5 +0,0 @@
-# Livro de Receitas :man_cook:
-
-### Olá bem vindo ao meu livro de receitas :wave:
-
-- Strogonoff de frango
